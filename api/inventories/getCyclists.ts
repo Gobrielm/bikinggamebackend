5e56de1f0df36031f0968c6e633897b02743aca4
Gabriel McKay <gabriel.p.mckay@gmail.com>
2025-10-14 16:43:08 -0700

    new functions for cyclists

---
+++ api/inventories/getCyclists.ts
@@ -0,0 +1,57 @@
+// Import the functions you need from the SDKs you need
+var admin = require("firebase-admin");
+import { getFirestore } from 'firebase-admin/firestore';
+import type { VercelRequest, VercelResponse } from '@vercel/node'
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const serviceAccount = {
+  "type": process.env.FIREBASE_TYPE,
+  "project_id": process.env.FIREBASE_PROJECT_ID,
+  "private_key_id": process.env.FIREBASE_KEY_ID,
+  "private_key": (process.env.FIREBASE_KEY || "").replace(/\\n/g, '\n'),
+  "client_email": process.env.FIREBASE_CLIENT_EMAIL,
+  "client_id": process.env.FIREBASE_CLIENT_ID,
+  "auth_uri": process.env.FIREBASE_AUTH_URI,
+  "token_uri": process.env.FIREBASE_TOKEN_URI,
+  "auth_provider_x509_cert_url": process.env.FIREBASE_AUTH_PROVIDER_CERT_URL,
+  "client_x509_cert_url": process.env.FIREBASE_CLIENT_CERT_URL,
+  "universe_domain": process.env.FIREBASE_DOMAIN
+};
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount)
+});
+
+const db = getFirestore('(default)');
+
+async function getCyclists(email: string, cyclist_id: number, cyclist_info: string) {
+    const docRef = await db.collection("inventories").doc(email);
+    const snapshot = await docRef.get();
+    if (!snapshot.exists) {
+        throw new Error("Can't find document for " + email);
+    }
+    return snapshot.data;
+}
+
+export default async function handler(req: VercelRequest, res: VercelResponse) {
+  try {
+    const email: string = req.body.email;
+    const cyclist_id: number = req.body.cyclist_id
+    const cyclist_info: string = req.body.cyclist_info
+    
+    const cyclists = await getCyclists(email, cyclist_id, cyclist_info);
+    
+    res.status(200).json({
+      message: "Setup User Successfully!",
+      cyclists: cyclists
+    });
+  } catch (err) {
+    console.error("Error creating data:", err);
+    res.status(500).json({ 
+      error: "Internal Server Error",
+      message: `Email: ${req.body.email}`
+    });
+  }
+}
